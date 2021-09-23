cd13dc3c546bf6750dd9b911dfb847d5ca20a7a7
Valerie Russell <vruss14@gmail.com>
2021-09-23 12:10:06 -0400

    correct and incorrect guesses work as expected

--- index.py
+++ index.py
@@ -1,16 +1,32 @@
 import random
-
 intro = input('Welcome to the game! Here\'s how this is going to work: You or I will guess a number from 1–10 and then the other will guess. Sound good? Just say yes or no to continue!').lower().lstrip().rstrip()
-print(intro)
 
 def choose_number():
     chosen_number = random.randint(1, 10)
-    print(chosen_number)
+    return chosen_number
 
+def ask_guess():
+    user_guess = input('Awesome! I\'ll go first. I\'m thinking of a number between 1–10. What do you think it is?')
+    cpu_choice = choose_number()
+    check_guess(user_guess, cpu_choice)
+
+def check_guess(user, cpu):
+    print(user)
+    print(cpu)
+    if(int(user) == int(cpu)):
+        print('Woohoo! That\'s right!')
+    else:
+        subsequent_guess(cpu)
+
+def subsequent_guess(cpu):
+    extra_guess = input('Haha! That\'s not it! Guess again!')
+    if(int(extra_guess) == int(cpu)):
+        print('Aw man! You got it!')
+    else:
+        subsequent_guess(cpu)
 
 if intro == 'yes':
-    input('Awesome! I\'ll go first. I\'m thinking of a number between 1–10. What do you think it is?')
-    choose_number()
+    ask_guess()
 elif intro == 'no':
     print('No worries! Hope I see you again soon!')
 else:
